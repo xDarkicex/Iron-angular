11e01c1b8da5cc47e02921ace7e779c39552de59
Adrian Boisclair <adrianboisclair@gmail.com>
2016-08-01 20:48:10 -0700

    Adds default route for / route

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,5 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/homes/index'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- app/controllers/homes_controller.js
+++ app/controllers/homes_controller.js
@@ -3,6 +3,10 @@
 angular.module('myApp.homes_controller', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: '../../views/homes/index.html',
+    controller: 'homeCtrl'
+  });
   $routeProvider.when('/home', {
     templateUrl: '../../views/homes/index.html',
     controller: 'homeCtrl'
